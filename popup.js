98dc4efb5bbad7c506d8105a270245cf473aafe9
Alla Hoffman <ahoffman@launchdarkly.com>
2022-05-05 10:35:54 -0400

    closes current ab

--- popup.js
+++ popup.js
@@ -1,14 +1,5 @@
 deleteTab.addEventListener("click", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    function: doTabDeletion,
-  });
+  chrome.tabs.remove(tab.id);
 });
-
-// The body of this function will be executed as a content script inside the
-// current page
-function doTabDeletion() {
-  console.log('delete tab logic')
-}
