cfcf71208cfc652cd260f96f9187e3131d7602dd
AllaHoffman <sm1th303@gmail.com>
2022-05-05 11:23:49 -0400

    fix await

--- background.js
+++ background.js
@@ -5,13 +5,13 @@ chrome.alarms.create("bananatime", {
 
 chrome.alarms.onAlarm.addListener(function(alarm) {
   if (alarm.name === "bananatime") {
-    tabslayer();
+    let tabs = await chrome.tabs.query({});
+    tabslayer(tabs);
   }
 });
 
 
-function tabslayer() {
-  let tabs = await chrome.tabs.query({});
+function tabslayer(tabs) {
   const doomedTab = tabs[Math.floor(Math.random() * tabs.length)];
   chrome.tabs.remove(doomedTab.id);
 }
