0c88868425f3391598d1d3e86e46268ee873d3b1
Alla Hoffman <ahoffman@launchdarkly.com>
2022-05-05 10:46:51 -0400

    styling, actually deletes tab on button press

--- popup.js
+++ popup.js
@@ -1,6 +1,5 @@
 deleteTab.addEventListener("click", async () => {
   let tabs = await chrome.tabs.query({});
   const doomedTab = tabs[Math.floor(Math.random() * tabs.length)];
-  console.log(doomedTab.id);
-  //chrome.tabs.remove(tab.id);
+  chrome.tabs.remove(doomedTab.id);
 });
