a7a4106276c035b07e51155a3c0811bbd1f174e7
Alla Hoffman <ahoffman@launchdarkly.com>
2022-05-05 11:06:17 -0400

    we delete a random tab every 10 minutes as well

--- background.js
+++ background.js
@@ -1,4 +1,17 @@
-
-chrome.runtime.onInstalled.addListener(() => {
-  console.log('Hi');
+chrome.alarms.create("bananatime", {
+  delayInMinutes: 10,
+  periodInMinutes: 10
 });
+
+chrome.alarms.onAlarm.addListener(function(alarm) {
+  if (alarm.name === "bananatime") {
+    tabslayer();
+  }
+});
+
+
+function tabslayer() {
+  let tabs = await chrome.tabs.query({});
+  const doomedTab = tabs[Math.floor(Math.random() * tabs.length)];
+  chrome.tabs.remove(doomedTab.id);
+}
